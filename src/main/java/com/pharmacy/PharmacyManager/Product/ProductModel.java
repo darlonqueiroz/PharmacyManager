6a4c9b6d725f043d3f79aa3cb8a4b65afd571b2b
darlonqueiroz <desenvolvedordecodigo@gmail.com>
2026-04-13 23:06:54 -0300

    Fix: Some attributes have been changed

--- src/main/java/com/pharmacy/PharmacyManager/Product/ProductModel.java
+++ src/main/java/com/pharmacy/PharmacyManager/Product/ProductModel.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Entity
@@ -23,7 +24,9 @@ public class ProductModel {
     @Column(name = "name_product")
     private String name;
     @Column (name = "price_product")
-    private int price;
+    private BigDecimal price;
+    @Column(name = "quantity_product")
+    private Integer quantity;
     @Column(name ="validity")
     private String validity;
     @Column(name ="description")
