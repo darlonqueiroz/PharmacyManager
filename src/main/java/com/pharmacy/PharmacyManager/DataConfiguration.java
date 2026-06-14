cca9e4d05e2f7330f5885f9ad946b92aeea6d8ba
darlonqueiroz <desenvolvedordecodigo@gmail.com>
2026-06-14 18:02:44 -0300

    Feat: created classs configuration for athenticate login

---
+++ src/main/java/com/pharmacy/PharmacyManager/DataConfiguration.java
@@ -0,0 +1,32 @@
+package com.pharmacy.PharmacyManager;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+public class DataConfiguration {
+    @Bean
+    public Datasource dataSource() {
+        // Configure and return the DataSource
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/database_login?useSSL=true&serverTimezone=UTC");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
+        return dataSource;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        // Configure and return the JPA Vendor Adapter
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.MYSQL);
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(true);
+        return adapter;
+    }
+}
