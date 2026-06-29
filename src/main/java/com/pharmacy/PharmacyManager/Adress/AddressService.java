dcc9fa37bd7ae4d2631bdc191dba6564d7532f38
darlonqueiroz <desenvolvedordecodigo@gmail.com>
2026-06-29 17:25:36 -0300

    Refactor: acrecent final in the injection dependency

--- src/main/java/com/pharmacy/PharmacyManager/Adress/AddressService.java
+++ src/main/java/com/pharmacy/PharmacyManager/Adress/AddressService.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AddressService {
-    private AddressRepository addressRepository;
+    private final AddressRepository addressRepository;
 
     public AddressService(AddressRepository addressRepository) {
         this.addressRepository = addressRepository;
