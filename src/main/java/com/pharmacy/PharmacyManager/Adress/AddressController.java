87e6a6728adfb032393ec29c0e116a3939c6ccc8
darlonqueiroz <desenvolvedordecodigo@gmail.com>
2026-07-02 21:15:11 -0300

    Feat: Created attributes

--- src/main/java/com/pharmacy/PharmacyManager/Adress/AddressController.java
+++ src/main/java/com/pharmacy/PharmacyManager/Adress/AddressController.java
@@ -1,5 +1,11 @@
 package com.pharmacy.PharmacyManager.Adress;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/address")
 public class AddressController {
+
 
 }
