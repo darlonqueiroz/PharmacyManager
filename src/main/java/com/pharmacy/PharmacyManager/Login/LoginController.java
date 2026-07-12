47d3fb189270a70ac4f5f16869db05840f0731c5
darlonqueiroz <desenvolvedordecodigo@gmail.com>
2026-07-12 14:51:47 -0300

    Fix: put bean in classe

--- src/main/java/com/pharmacy/PharmacyManager/Login/LoginController.java
+++ src/main/java/com/pharmacy/PharmacyManager/Login/LoginController.java
@@ -1,7 +1,11 @@
 package com.pharmacy.PharmacyManager.Login;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping ("/login")
 public class LoginController {
         private final LoginService loginService;
 
